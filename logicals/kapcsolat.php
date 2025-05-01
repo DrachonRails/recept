86deaa7cf6d042f87a7f11f524665798b80b544b
Balázs <ablonczi.92@gmail.com>
2025-05-01 21:06:11 +0200

    kapcs jav

--- logicals/kapcsolat.php
+++ logicals/kapcsolat.php
@@ -1,17 +1,12 @@
 <?php
-// Az űrlap adatainak szerver oldali validációja
-// ha nincs nev mező, vagy a tartalma kevesebb, mint 5 karakter
-if(!isset($_POST['nev']) || strlen($_POST['nev']) < 5)
-exit("Hibás név: ".$_POST['nev']); // befejezi az alkalmazást
-// Itt is a megadott reguláris kifejezéssel ellennőrzi az email-t
+if(isset($_POST['nev']) && strlen($_POST['nev']) < 5)
+exit("Hibás név: ".$_POST['nev']);
 $re = '/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/';
-if(!isset($_POST['email']) || !preg_match($re,$_POST['email']))
+if(isset($_POST['email']) && !preg_match($re,$_POST['email']))
 exit("Hibás email: ".$_POST['email']);
-// Ha nincs Üzenet szövegterület, vagy üres az adata
-if(!isset($_POST['szoveg']) || empty($_POST['szoveg']))
+if(isset($_POST['szoveg']) && empty($_POST['szoveg']))
 exit("Hibás szöveg: ".$_POST['szoveg']);
 echo "Kapott értékek: ";
-// Strukturáltan kiírja a $_POST tömb adatait.
 echo "<pre>";
 var_dump($_POST);
 echo "</pre>";
