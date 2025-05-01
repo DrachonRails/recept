979e79f4ed76532dc092958fd9ee3c724754a72e
DrachonRails <drachonrails@gmail.com>
2025-05-01 18:22:15 +0200

    first commit

---
+++ templates/index.tpl.php
@@ -0,0 +1,47 @@
+<?php session_start(); ?>
+<?php if(file_exists('./logicals/'.$keres['fajl'].'.php')) { include("./logicals/{$keres['fajl']}.php"); } ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?= $ablakcim['cim'] . ( (isset($ablakcim['mottó'])) ? ('|' . $ablakcim['mottó']) : '' ) ?></title>
+	<link rel="stylesheet" href="./styles/stilus.css" type="text/css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+	<?php if(file_exists('./styles/'.$keres['fajl'].'.css')) { ?><link rel="stylesheet" href="./styles/<?= $keres['fajl']?>.css" type="text/css"><?php } ?>
+</head>
+<body>
+	<header>
+		<img src="./images/<?=$fejlec['kepforras']?>" alt="<?=$fejlec['kepalt']?>">
+		<h1><?= $fejlec['cim'] ?></h1>
+		<?php if (isset($fejlec['motto'])) { ?><h2><?= $fejlec['motto'] ?></h2><?php } ?>
+		<?php if(isset($_SESSION['login'])) { ?>Bejlentkezve: <strong><?= $_SESSION['csn']." ".$_SESSION['un']." (".$_SESSION['login'].")" ?></strong><?php } ?>
+	</header>
+
+	<nav class="py-2 bg-body-tertiary border-bottom">
+		<div class="container d-flex flex-wrap">
+			<ul class="nav me-auto">
+				<?php foreach ($oldalak as $url => $oldal) { ?>
+					<?php if(! isset($_SESSION['login']) && $oldal['menun'][0] || isset($_SESSION['login']) && $oldal['menun'][1]) { ?>
+						<li class="nav-item" >
+						<a href="<?= ($url == '/') ? '.' : $url ?>" <?= (($oldal == $keres) ? ' class="nav-link link-body-emphasis px-2 active"' : '') ?> <?= (($oldal != $keres) ? ' class="nav-link link-body-emphasis px-2"' : '') ?>>
+						<?= $oldal['szoveg'] ?></a>
+						</li>
+					<?php } ?>
+				<?php } ?>
+			</ul>
+		</div>
+	</nav>
+
+<div id="content">
+	<?php include("./templates/pages/{$keres['fajl']}.tpl.php"); ?>
+</div>
+
+    <footer>
+        <?php if(isset($lablec['copyright'])) { ?>&copy;&nbsp;<?= $lablec['copyright'] ?> <?php } ?>
+		&nbsp;
+        <?php if(isset($lablec['ceg'])) { ?><?= $lablec['ceg']; ?><?php } ?>
+    </footer>
+</body>
+</html>
