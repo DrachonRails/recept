5d16dd9d06344e19d86e03a0939309fa1bb1e79d
Balázs <ablonczi.92@gmail.com>
2025-05-03 11:52:42 +0200

    uzenet fix

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -20,7 +20,7 @@ $oldalak = array(
     'galeria' => array('fajl' => 'galeria', 'szoveg' => 'Galeria', 'menun' => array(1,1)),
     'feltoltes' => array('fajl' => 'feltoltes', 'szoveg' => 'Feltoltes', 'menun' => array(0,1)),
 	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
-    'uzenetek' => array('fajl' => 'uzenetek', 'szoveg' => 'Üzenetek', 'menun' => array(1,1)),
+    'uzenetek' => array('fajl' => 'uzenetek', 'szoveg' => 'Üzenetek', 'menun' => array(0,1)),
     'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
     'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
     'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
